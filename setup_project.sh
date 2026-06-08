f650ef0a972c8a16f8561ba899eb1567fc2e5dc5
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-06-08 12:47:12 +0300

    feat: partially complete the initiliaze function for the agent

--- setup_project.sh
+++ setup_project.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+TEMPLATE_DIR="template"
+PROJECT_NAME="attendance_tracker"
+
+if [ -d "$TEMPLATE_DIR"]; then
+    echo "The source code for the student attandance tracker project is missing"
+    exit
+fi
+
+archive_continue() {}
+
+init_system() {
+    clear
+    
+    read -p "Enter the variation of this project: " PROJ_VARIATION
+
+    WORKSPACE="${PROJECT_NAME}_${PROJ_VARIATION}"
+    
+    if [ -d "$WORKSPACE" ]; then
+        read -p "This workspace  $WORKSPACE already exists. Do you want to overwrite it? [Y / N]"
+        archive_continue PROJ_VARIATION
+    else
+	echo " Starting to create a new workspace => $WORKSPACE ......"
+
+	
+    fi
+}
